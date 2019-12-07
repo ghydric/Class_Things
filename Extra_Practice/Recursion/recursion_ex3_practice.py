54289c4dc3b600400fed1f8be44b6815316d2d96
Gary Hydrick <gary.hydrickm@gmail.com>
2019-12-06 21:31:51 -0600

    completed recursion_ex3_practice.py

---
+++ Extra_Practice/Recursion/recursion_ex3_practice.py
@@ -0,0 +1,13 @@
+"""
+3. Recursive Lines
+    Write a recursive function that accepts an integer argument, n. The function should display
+    n lines of asterisks on the screen, with the first line showing 1 asterisk, the second line
+    showing 2 asterisks, up to the nth line which shows n asterisks.
+"""
+
+def rec_lines(n):
+    if n > 1:
+        rec_lines(n-1)
+    print("*" * n)
+    
+rec_lines(6)
